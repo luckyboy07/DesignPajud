4f60b67a6ff681076844e3351f28a981c3fe6961
CairudinRamos <cairudin.16@gmail.com>
2016-04-11 13:34:34 +0800

    routes

--- app/myApp.js
+++ app/myApp.js
@@ -9,11 +9,23 @@ angular.module('myApp', ['ui.router'])
                 templateUrl: 'app/view/supplier.html',
 
             })
-             .state('analytic', {
-             	url: '/analytic',
-             	templateUrl: 'app/view/Analytic/analytics.html',
-             	controller: 'analyticCtrl'
-             });
+            .state('analytic', {
+                url: '/analytic',
+                templateUrl: 'app/view/Analytic/analytics.html',
+                controller: 'analyticCtrl'
+            })
+            .state('deploy', {
+                url: '/deploy',
+                templateUrl: 'app/view/Deploy/deploy.html',
+            })
+            .state('package', {
+                url: '/package',
+                templateUrl: 'app/view/Package/package.html',
+            })
+            .state ('user', {
+                url: '/user',
+                templateUrl: 'app/view/Users/user.html'
+            })
 
         $urlRouterProvider.otherwise('/supplier');
 
